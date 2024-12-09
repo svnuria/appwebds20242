77012dc690231b9fd5800ac7e34ed8551c3e204c
Yurika2002 <vidalinayurika@gmail.com>
2024-12-09 17:25:44 -0500

    feat: Incializacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PersonService } from './api/person.service';
+import { BsModalService } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
+  providers: [
+	BsModalService
+],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.sass'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'appwebds20242';
+  title = 'hola mundo';
+  person: any = {};
+
+  constructor(
+		private personService: PersonService,
+		private modalService: BsModalService
+	) { }
+	ngOnInit() {
+		this.personService.getData().subscribe({
+			next: (response: any) => {
+				this.person = response;
+			},
+			error: (error: any) => {
+				console.log(error);
+			}
+		});
+	}
+
+	showModal(myModal: TemplateRef<any>): void {
+		this.modalService.show(myModal);
+	}
+	closeModal(): void {
+		this.modalService.hide();
+	}
 }
+
